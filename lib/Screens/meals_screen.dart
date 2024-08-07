316191ab7eac2a91a5e72e5d688fc97b43f3c374
Shaheen <AiExpert2025@gmail.com>
2024-08-07 11:08:12 +0300

    adding appbar to meals

--- lib/Screens/meals_screen.dart
+++ lib/Screens/meals_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/models/meal.dart';
 
+import 'package:meals/models/meal.dart';
 import 'package:meals/providers/meals_provider.dart';
+import 'package:meals/providers/selected_category_provider.dart';
 
 class MealsScreen extends ConsumerWidget {
   const MealsScreen({super.key});
@@ -10,8 +11,10 @@ class MealsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final meals = ref.watch(mealsProvider);
-    return ListView(
-      children: [for (Meal meal in meals) Text(meal.title)],
+    final category = ref.watch(selectedCategoryProvider);
+    return Scaffold(
+      appBar: AppBar(title: Text(category?.title ?? "All categories")),
+      body: ListView(children: [for (Meal meal in meals) Text(meal.title)]),
     );
   }
 }
